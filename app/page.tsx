34f7977d764eeff0cc91dda0e923b9389aee6a7f
Bhawesh <bhaweshp17@gmail.com>
2025-03-22 20:34:14 +0530

    ProductDetails page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Carousel, Card, Button, Row, Col, Typography } from 'antd';
-import 'animate.css';
-import "@/styles/page.scss"
-import Image from 'next/image';
-import '@ant-design/icons'
-
+import React from "react";
+import { Carousel, Card, Button, Row, Col, Typography } from "antd";
+import "animate.css";
+import "@/styles/page.scss";
+import Image from "next/image";
+import "@ant-design/icons";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
@@ -25,24 +25,26 @@ const Home: React.FC = () => {
           {[1, 1, 1, 2, 2, 2].map((item, index) => {
             return (
               <Col span={8} key={index}>
-                <Card
-                  hoverable
-                  cover={
-                    <div style={{ textAlign: 'center' }}>
-                      <Image
-                        alt="product1"
-                        src="https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg?auto=compress&cs=tinysrgb&w=600"
-                        width={300}
-                        height={200}
-                      />
+                <Link href={`/product-details`}>
+                  <Card
+                    hoverable
+                    cover={
+                      <div style={{ textAlign: "center" }}>
+                        <Image
+                          alt="product1"
+                          src="https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg?auto=compress&cs=tinysrgb&w=600"
+                          width={300}
+                          height={200}
+                        />
+                      </div>
+                    }
+                  >
+                    <div>
+                      <h3>Product 1</h3>
+                      <p>$20.00</p>
                     </div>
-                  }
-                >
-                  <div>
-                    <h3>Product 1</h3>
-                    <p>$20.00</p>
-                  </div>
-                </Card>
+                  </Card>
+                </Link>
               </Col>
             );
           })}
@@ -57,26 +59,33 @@ const Home: React.FC = () => {
             <div key={index}>
               <Row justify={"center"} gutter={[16, 16]}>
                 {[1, 2, 3].map((_, innerIndex) => (
-                  <Col span={8} key={innerIndex} >
-                    <Card
-                      hoverable
-                      cover={
-                        <div style={{ justifyContent: 'center', display: "flex" }}>
-                          <Image
-                            style={{ marginTop: "10px" }}
-                            alt={`product${innerIndex + 1}`}
-                            src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600"
-                            width={300}
-                            height={200}
-                          />
+                  <Col span={8} key={innerIndex}>
+                    <Link href={`/product-details`}>
+                      <Card
+                        hoverable
+                        cover={
+                          <div
+                            style={{
+                              justifyContent: "center",
+                              display: "flex",
+                            }}
+                          >
+                            <Image
+                              style={{ marginTop: "10px" }}
+                              alt={`product${innerIndex + 1}`}
+                              src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600"
+                              width={300}
+                              height={200}
+                            />
+                          </div>
+                        }
+                      >
+                        <div>
+                          <h3>Product {innerIndex + 1}</h3>
+                          <p>$20.00</p>
                         </div>
-                      }
-                    >
-                      <div>
-                        <h3>Product {innerIndex + 1}</h3>
-                        <p>$20.00</p>
-                      </div>
-                    </Card>
+                      </Card>
+                    </Link>
                   </Col>
                 ))}
               </Row>
@@ -93,11 +102,13 @@ const Home: React.FC = () => {
             <div key={index}>
               <Row justify={"center"} gutter={[16, 16]}>
                 {[1, 2, 3].map((_, innerIndex) => (
-                  <Col span={8} key={innerIndex} >
+                  <Col span={8} key={innerIndex}>
                     <Card
                       hoverable
                       cover={
-                        <div style={{ justifyContent: 'center', display: "flex" }}>
+                        <div
+                          style={{ justifyContent: "center", display: "flex" }}
+                        >
                           <Image
                             style={{ marginTop: "10px" }}
                             alt={`product${innerIndex + 1}`}
@@ -129,11 +140,13 @@ const Home: React.FC = () => {
             <div key={index}>
               <Row justify={"center"} gutter={[16, 16]}>
                 {[1, 2, 3].map((_, innerIndex) => (
-                  <Col span={8} key={innerIndex} >
+                  <Col span={8} key={innerIndex}>
                     <Card
                       hoverable
                       cover={
-                        <div style={{ justifyContent: 'center', display: "flex" }}>
+                        <div
+                          style={{ justifyContent: "center", display: "flex" }}
+                        >
                           <Image
                             style={{ marginTop: "10px" }}
                             alt={`product${innerIndex + 1}`}
