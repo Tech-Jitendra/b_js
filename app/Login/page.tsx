26acb2e919a10209bf3687c8b37ed91c7ee7ce95
jitendra-maurya <jitenmaurya0@gmail.com>
2025-03-24 23:05:49 +0530

    Enhance login experience with confetti animation on successful login and update dependencies

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Input, message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../redux/slices/userSlice';
 import { RootState } from '../../redux/store';
+import Confetti from 'react-confetti';
+import { useWindowSize } from 'react-use';
 import 'animate.css';
 import "@/styles/login.scss";
 
 const LoginPage: React.FC = () => {
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state: RootState) => state.user);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const { width, height } = useWindowSize(); // Get window dimensions for confetti
 
   const onFinish = async (values: { email: string; password: string }) => {
     try {
@@ -24,6 +28,8 @@ const LoginPage: React.FC = () => {
         // Dispatch the login action
         dispatch(login(userData));
         message.success('Login successful!');
+        setShowConfetti(true); // Trigger confetti
+        setTimeout(() => setShowConfetti(false), 5000); // Stop confetti after 5 seconds
       } else {
         throw new Error('Invalid email or password');
       }
@@ -34,6 +40,9 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="login-page animate__animated animate__fadeIn">
+      {/* Confetti Animation */}
+      {showConfetti && <Confetti width={width} height={height} />}
+
       {/* Floating Gifts */}
       <div className="floating-gift"></div>
       <div className="floating-gift"></div>
@@ -41,12 +50,7 @@ const LoginPage: React.FC = () => {
       <div className="floating-gift"></div>
 
       <div className="login-container">
-        <div className="logo">
-            {/* <h1> */}
-                Trio Trendz
-
-            {/* </h1> */}
-        </div>
+        <div className="logo">Trio Trendz</div>
         <h2 className="title">Login</h2>
         {isLoggedIn ? (
           <p>You are already logged in!</p>
